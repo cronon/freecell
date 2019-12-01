b0b6c1634ee26edf040dc4552432e6fe8e33c230
cronon <cronon12@gmail.com>
2019-12-01 20:17:39 +0300

    Updates

---
+++ precache-manifest.a262e7cf8ed7e935d875a8847af10598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b346d20219469af62ec59ed720081d6",
+    "url": "/freecell/index.html"
+  },
+  {
+    "revision": "b2f38a04f072302ab086",
+    "url": "/freecell/static/css/main.3ec777ed.chunk.css"
+  },
+  {
+    "revision": "4559c993a4251e9c8a53",
+    "url": "/freecell/static/js/2.13eb5988.chunk.js"
+  },
+  {
+    "revision": "b2f38a04f072302ab086",
+    "url": "/freecell/static/js/main.40038a9a.chunk.js"
+  },
+  {
+    "revision": "ed9821a4f7705c0c5347",
+    "url": "/freecell/static/js/runtime-main.9942e77f.js"
+  }
+]);
